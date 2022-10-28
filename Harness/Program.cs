24864b9c7fe15e95058c690752a071c12a2b4cbd
JD Kilgallin <jkilgall@keyfactor.com>
2022-10-28 14:23:16 -0400

    Commit client SDK code

---
+++ Harness/Program.cs
@@ -0,0 +1,35 @@
+﻿using Keyfactor;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Harness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client c = new Client("http://192.168.78.139/KeyfactorAPI", "RE9NQUlOXHVzZXI6UGFzc3dvcmQ=");
+
+            PFXEnrollmentResponse pfxResp = c.PostPFXEnrollAsync("PFX", new PFXEnrollmentRequest()
+            {
+                Subject="CN=CSharpTest",
+                Timestamp = DateTime.UtcNow,
+                Template = "WebServer",
+                IncludeChain = false,
+                PopulateMissingValuesFromAD = false
+            }).Result;
+            Console.WriteLine(pfxResp.CertificateInformation.Thumbprint);
+
+            c.UpdateMetadataAsync(null, new MetadataUpdateRequest()
+            {
+                Id = pfxResp.CertificateInformation.KeyfactorId.Value,
+                Metadata = new Dictionary<string, string>() { { "Email-Contact", "keyfactor@example.com" } }
+            });
+
+            CertificateRetrievalResponse resp = c.GetCertificateAsync(pfxResp.CertificateInformation.KeyfactorId.Value, null, null, null, 4).Result;
+            Console.WriteLine(Convert.ToBase64String(resp.ContentBytes));
+            Console.WriteLine(resp.Metadata["Email-Contact"]);
+        }
+    }
+}
